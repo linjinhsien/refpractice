322dea31e4ab4ff4b1970296244eb99913cd8c31
linjinhsien <72392382+linjinhsien@users.noreply.github.com>
2024-07-10 20:22:56 +0800

    Update

---
+++ Ex1.kt
@@ -0,0 +1,12 @@
+package ch4
+
+import jdk.swing.interop.SwingInterOpUtils
+
+fun main() {
+    for(j in 1..9){
+    for(i in 1..9){print(j*i)
+     print("\t")
+    }
+        print("\n")
+    }
+}
